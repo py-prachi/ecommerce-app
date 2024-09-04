a8980e6447ee0c790471d5d4c16a43ebf804f1fe
Prachi Jagtap <prachijagtap@Prachis-MacBook-Pro.local>
2024-09-04 16:37:10 +0530

    added search

--- src/app/product-dashboard/product-dashboard.component.ts
+++ src/app/product-dashboard/product-dashboard.component.ts
@@ -3,42 +3,65 @@ import { Product } from '../model/productInterface';
 import { ProductService } from '../product.service';
 import { MatIconModule } from '@angular/material/icon'
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-product-dashboard',
   standalone: true,
-  imports: [CommonModule, MatIconModule],
+  imports: [CommonModule, MatIconModule, FormsModule],
   templateUrl: './product-dashboard.component.html',
   styleUrl: './product-dashboard.component.css'
 })
 export class ProductDashboardComponent implements OnInit{
+
   
   products: Product[]=[];
   displayedProducts: Product [] = [];
+  filteredProducts: Product [] = [];
   currentPage = 1;
   pageSize = 10;
   totalPages = 0;
+  searchQuery = '';
 
   constructor(private productService:ProductService){}
 
   ngOnInit(): void {
     this.products = this.productService.getProducts()
-    this.totalPages = Math.ceil(this.products.length / this.pageSize)
+    this.filteredProducts = [...this.products]
+    this.totalPages = Math.ceil(this.filteredProducts.length / this.pageSize)
     this.updateDisplayedProducts();
   }
+
+  onSearch(): void {
+    const query = this.searchQuery.trim().toLowerCase();
+    console.log("triggering search...",query)
+    if(query){
+      this.filteredProducts = this.products.filter(product =>
+        product.name.toLowerCase().includes(query) 
+      );
+     console.log("The list: ",this.filteredProducts) 
+    
+    }else{
+      this.filteredProducts = [...this.products]
+    }
+    
+    
+  
+    this.currentPage = 1;
+    this.totalPages = Math.ceil(this.filteredProducts.length / this.pageSize)
+    this.updateDisplayedProducts();
+    }
   
   onDelete(product:Product){
     this.products = this.products.filter(p=>p.id !== product.id)
-    this.totalPages = Math.ceil(this.products.length / this.pageSize)
-    this.updateDisplayedProducts();
-    console.log('deleted from product list:', product)
+    this.onSearch();
 
   }
 
   updateDisplayedProducts() :void{
     const startIndex = (this.currentPage - 1) * this.pageSize
     const endIndex = startIndex + this.pageSize;
-    this.displayedProducts = this.products.slice(startIndex,endIndex)
+    this.displayedProducts = this.filteredProducts.slice(startIndex,endIndex)
     }
   
     goToPage(page: number): void {
